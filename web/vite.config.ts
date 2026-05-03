249fd0543328195bc66ba195dc4dfb3a05c92704
ohjoonhee <ohjoonhee@snu.ac.kr>
2026-05-04 02:31:59 +0900

    chore: add Vitest with jsdom

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/lickipedia/',
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
